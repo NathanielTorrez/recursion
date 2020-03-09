092cc43b91ffda496b23814da4704c3f2e87b24c
Nathaniel Torrez <nathanieltorrez@icloud.com>
2020-03-09 15:45:59 -0700

    Solved getElementsbyClassName
    Built a passing function for the first of 2 in recursion. Built 2 seperate functions that pass given test. Both use recursion but one is using recursion on a built in helper function the other is using the given function itself for recursion.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,52 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	
+
+  // var results = []; // create results variable
+
+  // create a helper function to check if it has a class list and if that contains the className
+  
+  // var elementSearch = function(element){
+
+  //   if(element.classList && element.classList.contains(className)){
+  //       results.push(element); // > this keeps track of the results and serves as the break condition
+  //     }
+
+  // create a loop to iterate over all of the child nodes under the parent
+
+  //   for(var i = 0; i < element.childNodes.length; i++){
+  //     elementSearch(element.childNodes[i]); // > recursion over the childNodes
+  //   }
+  // }
+
+  //  elementSearch(document.body);
+  //  return results;
+
+  //----------------------------------------------------------------------
+  // better solution below
+
+  // create result array
+	
+	var result = [];
+
+	// The element you are looking at is the body at first but,
+	// we are going to want to look at the argument we supply during recursion
+	var element = arguments[1] || document.body ;
+
+ 	// if it has a classlist and it contains the className then we want to save it
+	if (element.classList && element.classList.contains(className)) {
+		result.push(element)
+	}
+
+	// now we need to go through all the childNodes using a for loop
+
+		for (var i = 0; i < element.childNodes.length; i++) {
+			// result is now going to equal the results of running through all nodes and child nodes
+			result = result.concat(getElementsByClassName( className, element.childNodes[i]))
+		}
+	// return results for the answer
+	
+	return result
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+
+  // solve for the one answer cases
+  var string = "";
+
+  if (typeof obj === "boolean" || typeof obj === "number") {
+  	return string += obj 
+  } else if (typeof obj === "null") {
+  	return "null"
+  } else if ( typeof obj === "undefined") {
+  	return "undefined"{
+  } else if (typeof obj === 'string') {
+  	return obj;
+  }
+
+  // create circumstances for is it  an object and if it is an array like object
+
+  if (typeof obj === 'object' and Array.isArray(obj) === false) {
+   var opener = '{';
+   var closer = '}'
+  } else if (Array.isArray) {
+  	var opener = '['
+  	var closer = ']'
+  }
+
+// find out of the object has any elements in it to know when to end
+// maybe use a for loop or a function to iterate over the contents
+
+
+
 };
